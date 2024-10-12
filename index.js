f6b28c4f75306cd1bb09e492d1c44b09dda3602c
RakanMyHusbando <hellwing.jonas@web.de>
2024-10-12 16:09:05 +0200

    fixed all

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ const fs = require("fs")
 
 const BASE_URL = "http://127.0.0.1:5000/api"
 
-const url = (path=[],vars=[]) => {
+const url = (path,vars) => {
     let url = BASE_URL
     if(path.length > 0)
-        for(const elem in path)
+        for(const elem of path)
             url += `/${elem}`
     if(vars.length > 0)
         for(let i=0;i<vars.length;i++)
             url += `${i == 0 ? "?" : "&"}${vars[i][0]}=${vars[i][1]}`
+    return url
 }
 
 const post = async (url,body) => {
     try {
+        console.log(body)
         const response = await fetch(url, {
             method: "POST",
             body: JSON.stringify(body)
-        }).then(res=>res.status)
-        if(response.status == 201)
-            return `${new Date()}; ${url}; ${response}`
+        })
+        return `${new Date()}; ${url}; ${response.status}`
     } catch (err) {
         return `${new Date()}; ${url}; 400`
     }
@@ -27,26 +28,29 @@ const post = async (url,body) => {
 
 const get = async (url) => {
     try {
-        const response = await fetch(url).then(res=>res.status)
-        if(response.status == 201)
-            return `${new Date()}; ${url}; ${response}`
+        const response = await fetch(url)
+        return `${new Date()}; ${url}; ${response.status}`
     } catch (err) {
         return `${new Date()}; ${url}; 400`
     }
 } 
 
 const init = async () => {
-    const result = ""
+    let result = ""
     const errors = []
     const requests = JSON.parse(fs.readFileSync("requests.json"))
-    
+
     for(const req of requests){
         const this_url = url(req.path,req.vars)
+        console.log(this_url)
         try {
-            if(req.body)
-                result += await post(this_url, req.body) + "\n"
-            else
-                result += await get(this_url) + "\n"
+            if(req.body){
+                const postReq = await post(this_url, req.body)
+                result += postReq + "\n"
+            } else {
+                const getReq = await get(this_url)
+                result += getReq + "\n"
+            }
         } catch (err) {
             console.log(err)
             errors.push({
@@ -57,7 +61,7 @@ const init = async () => {
     }
 
     fs.writeFileSync("result.csv",result)
-    fs.writeFileSync("error.json",JSON.stringify(result))
+    fs.writeFileSync("error.json",JSON.stringify(errors))
 }
 
 init()
