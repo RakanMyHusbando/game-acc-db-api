1bc12a746687e9fcac30ac9741c86ca4acdbf85e
RakanMyHusbando <hellwing.jonas@web.de>
2024-10-14 08:44:23 +0200

    addet models.team.Discord().create() and changed team_discod TABLE

--- src/services/team.py
+++ src/services/team.py
@@ -22,29 +22,12 @@ class Team:
         self.model = model.Team()
 
     def create(self,input) -> str|None:
-        member = None
-        if utils.try_key(input,"member"):   
-            member = [] 
-            for key in input["member"]:
-                if key == "main":
-                    for role in input["member"][key]:
-                        member.append([
-                            input["member"][key][role],
-                            f'main_{role}'
-                        ])
-                if key == "substitute":
-                    for elem in input["member"][key]:
-                        member.append([
-                            elem["name"],
-                            f'substitute_{elem["role"]}'
-                        ])
-                elif type(input["member"][key]) == str:
-                    member.append([input["member"][key],key])
+        
         return self.model.creat(
             utils.try_key(input,"team_name"),
             utils.try_key(input,"game"),
             utils.try_key(input,"guild_name"),
-            member
+            utils.member_handle_json(input)
         )
     
     def get(self,input) -> list|None:
--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,26 @@ class UtilsServices:
             return result
         except:
             return None
+    def member_handle_json(self,input):
+        member = None
+        if self.try_key(input,"member"):   
+            member = [] 
+            for key in input["member"]:
+                if key == "main":
+                    for role in input["member"][key]:
+                        member.append([
+                            input["member"][key][role],
+                            f'main_{role}'
+                        ])
+                if key == "substitute":
+                    for elem in input["member"][key]:
+                        member.append([
+                            elem["name"],
+                            f'substitute_{elem["role"]}'
+                        ])
+                elif type(input["member"][key]) == str:
+                    member.append([input["member"][key],key])
+        return member
     
 class UtilsModels: 
     def __init__(self,conn:Connection) -> None:
