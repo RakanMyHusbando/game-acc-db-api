10f980592c8023d2c92c26f0af731b879f07af5b
RakanMyHusbando <hellwing.jonas@web.de>
2024-10-17 08:09:02 +0200

    properties/* -> services.py

--- src/services.py
+++ src/services.py
@@ -1,30 +1,36 @@
 from utils import UtilsServices 
-from models.team import Team as model_team
-from models.user import User as model_user
-import properties.team as prop_team
-import properties.user as prop_user
+from models import team, user, guild
 
 utils = UtilsServices()
 
 class Team: 
     def __init__(self) -> None:
-        self.team = model_team()
-        self.discord = prop_team.Discord()
+        self.model = team
 
     def create(self,body,create) -> str|None:
         result = None
         match create:
             case None:
-                result = self.team.create(
+                result = self.model.Team().create(
                     utils.try_key(body,"team_name"),
                     utils.try_key(body,"game"),
                     utils.try_key(body,"guild_name"),
                     utils.member_handle_json(body)
                 )
             case "user":
-                pass
+                result = self.model.User().create(
+                    utils.try_key(body,"user_name"),
+                    utils.try_key(body,"team_name"),
+                    utils.try_key(body,"role")
+                )
             case "discord":
-                self.discord.create(body)
+                result = self.model.Discord().create(
+                    utils.try_key(body,"team_name"),
+                    utils.try_key(body,"server"),
+                    utils.try_key(body,"category"),
+                    utils.try_key(body,"roles"),
+                    utils.try_key(body,"channels")
+                )
         return utils.res_post(result)
 
     def get(self,search,props=[]) -> list|None:
@@ -40,26 +46,28 @@ class Team:
                         result_p = self.discord.get(search) 
                 if result_p:
                     result[name] = result_p
+        return utils.res_get(result)
 
 class User:
     def __init__(self) -> None:
-        self.user = model_user()
-        self.league_of_legends = prop_user.LeagueOfLegends()
-        self.valorant = prop_user.Valorant()
+        self.model = user
         
     def create(self,body,create) -> str|None:
         result = None
         match create:
+            case None:
+                result = self.model.User().create(
+                    utils.try_key(body,"user_name"),
+                    utils.try_key(body,"discord_id")
+                )
             case "league_of_leagends":
-                pass
-            case "valorant":
-                pass
-        if result == None:
-            result = self.user.create(
-                utils.try_key(body,"user_name"),
-                utils.try_key(body,"discord_id")
-            )
-        return result
+                result = self.model.LeagueOfLegends().create(
+                    utils.try_key(body,"user_name"),
+                    utils.try_key(body,"ingame_name"),
+                    utils.try_key(body,"position"),
+                    utils.try_key(body,"discord_id")
+                )
+        return utils.res_post(result)
     
     def get(self,search,props=[]) -> list|None:
         result = self.user.get(search)
@@ -76,3 +84,4 @@ class User:
                         result_p = self.valorant.get(search)
                 if result_p:
                     result[name] = result_p
+        return utils.res_get(result)
